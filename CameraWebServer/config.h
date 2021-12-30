f3726e714329edef0cb031fd8b69e451b5104d35
sigmdel <none@none>
2021-12-30 17:47:39 -0400

    Defining CONFIG_FLASH_LED in a board definition with automatically define CONFIG_LED_ILLUMINATOR_ENABLED Directive HAS_GRAB_MODE used in the camera setup in the setup() routine is defined here

--- CameraWebServer/config.h
+++ CameraWebServer/config.h
@@ -2,45 +2,49 @@
 #define CONFIG_H
 
 #include <esp_camera.h>
+#include "core_version.h" // to set grab_mode in latest ESP32 arduino core
 
 /*********************************************************************************
 Supported camera models
 
- WARNING! A PSRAM IC is required for UXGA resolution and high JPEG quality
- otherwise only partial images will be transmitted if they exceed the buffer size
+ WARNING! Select the correct board definition in the Arduino IDE or
+          set the correct default_envs in platformio.ini and define the correct
+          [env:xxx] if needed.
+
+ WARNING! PSRAM is required for UXGA resolution and high JPEG quality otherwise
+          only partial images will be transmitted if they exceed the buffer size.
 *********************************************************************************/
 
 // Select one and only one module
 //
 //#define CAMERA_MODEL_WROVER_KIT         //has PSRAM
 //#define CAMERA_MODEL_ESP_EYE            //has PSRAM
-//#define CAMERA_MODEL_AI_THINKER           //has PSRAM
+#define CAMERA_MODEL_AI_THINKER           //has PSRAM
 //#define CAMERA_MODEL_M5STACK_PSRAM      //has PSRAM (M5Camera version A?)
 //#define CAMERA_MODEL_M5STACK_V2_PSRAM   //has PSRAM (M5Camera version B?)
 //#define CAMERA_MODEL_M5STACK_WIDE       //has PSRAM (M5CameraF? )
 //#define CAMERA_MODEL_M5STACK_ESP32CAM   //no PSRAM
 //#define CAMERA_MODEL_M5STACK_UNITCAM    //no PSRAM
 //#define CAMERA_MODEL_TTGO_T_JOURNAL     //no PSRAM
-#define CAMERA_MODEL_CUSTOM_CAM
+//#define CAMERA_MODEL_CUSTOM_CAM
 
 
 // Board specific configuration
 
 #if defined(CAMERA_MODEL_AI_THINKER)
-  #define CONFIG_LED_ILLUMINATOR_ENABLED // Enables the flash LED. Do not define if there is no flash LED
-  #if defined(CONFIG_LED_ILLUMINATOR_ENABLED)
-    #define CONFIG_FLASH_LED 4
-    #define CONFIG_LED_LEDC_CHANNEL  LEDC_CHANNEL_7  // Channel 1 to 7 possible, 0 is used by camera
-  #endif
-  #define CONFIG_DEFAULT_RESOLUTION FRAMESIZE_SVGA   // 800x600
+  // Flash LED configuration
+  #define CONFIG_FLASH_LED 4
+  #define CONFIG_LED_LEDC_CHANNEL  LEDC_CHANNEL_7   // Channel 0 is used by camera
+
+  // camera settings at startup
+  #define CONFIG_DEFAULT_RESOLUTION FRAMESIZE_SVGA  // 800x600
   #define CONFIG_DEFAULT_QUALITY 4
   //#define CONFIG_V_FLIP
   //#define CONFIG_H_MIRROR
 #endif
 
 #if defined(CAMERA_MODEL_CUSTOM_CAM)
-  //#define BOARD_HAS_PSRAM            // with PSRAM on board, but there is a problem with PSRAM on pre-revision 3 ESP32 chips, add the following fix
-  //-mfix-esp32-psram-cache-issue      // Not needed with ESP32 revision 3. See https://github.com/espressif/esp-idf/issues/2892#issuecomment-754547748
+  // custom camera pin configuration
   #define CONFIG_CAMERA_PIN_PWDN  32
   #define CONFIG_CAMERA_PIN_RESET -1
   #define CONFIG_CAMERA_PIN_XCLK  0
@@ -57,12 +61,14 @@ Supported camera models
   #define CONFIG_CAMERA_PIN_VSYNC 25
   #define CONFIG_CAMERA_PIN_HREF  23
   #define CONFIG_CAMERA_PIN_PCLK  22
-  #if defined(CONFIG_LED_ILLUMINATOR_ENABLED)
-    #define CONFIG_FLASH_LED 4
-    #define CONFIG_LED_LEDC_CHANNEL  LEDC_CHANNEL_7  // Channel 1 to 7 possible, 0 is used by camera
-  #endif
-  //#define CONFIG_DEFAULT_RESOLUTION FRAMESIZE_SVGA   // 800x600
-  //#define CONFIG_DEFAULT_QUALITY 4
+
+  // Flash LED configuration
+  //#define CONFIG_FLASH_LED 4
+  //#define CONFIG_LED_LEDC_CHANNEL  LEDC_CHANNEL_7  // channel 0 is used by camera
+
+  // camera settings at startup
+  #define CONFIG_DEFAULT_RESOLUTION FRAMESIZE_SVGA   // 800x600
+  #define CONFIG_DEFAULT_QUALITY 4
   //#define CONFIG_V_FLIP
   //#define CONFIG_H_MIRROR
 #endif
@@ -70,16 +76,20 @@ Supported camera models
 
 // common configuration
 
-#define CONFIG_BAUD 115200                        // Mandatory.
-#define CONFIG_WIFI_SSID "your-ssid"              // Mandatory.
+#define CONFIG_BAUD 115200                        // PlatformIO: set monitor_speed to the same value
+#define CONFIG_WIFI_SSID "your-ssid"              // Mandatory
 #define CONFIG_WIFI_PWD  "your-password"          // Mandatory
 
 //#define CORE_DEBUG_LEVEL ARDUHAL_LOG_LEVEL_INFO // Optional. Default is ARDUHAL_LOG_LEVEL_ERROR see https://thingpulse.com/esp32-logging/
 //#define CONFIG_STATIC_IP_ENABLED                // Optional. If not defined the IP is obtained from the DHCP server
-//#define CONFIG_SHOW_NETWORK_PARAMS              // Optional.
+//#define CONFIG_SHOW_NETWORK_PARAMS              // Optional
 //#define CONFIG_MDNS_ADVERTISE_ENABLED           // Optional. If mDNS is enabled, the default hostname is "esp32-cam.local"
 //#define CONFIG_ESP_FACE_DETECT_ENABLED          // Optional. Works at low resolution <= 320x240
 //#define CONFIG_ESP_FACE_RECOGNITION_ENABLED     // Optional. Works at low resolution <= 320x240
+
+#if defined(ARDUINO_ESP32_RELEASE_2_0_0) || defined(ARDUINO_ESP32_RELEASE_2_0_2)
+#define HAS_GRAB_MODE
+#endif
 
 #if defined(CONFIG_STATIC_IP_ENABLED)
 #define CONFIG_STATICIP "192.168.1.27"
@@ -93,10 +103,28 @@ Supported camera models
 //#define CONFIG_LOCAL_HOSTNAME esp32-cam-01"     // Optional. Custom local hostname. Here "esp32-cam-01.local"
 #endif
 
-#if defined(CONFIG_LED_ILLUMINATOR_ENABLED)
-#define CONFIG_FLASH_PWM_FREQ    50000            //   - Mandatory if CONFIG_LED_ILLUMINATOR_ENABLED defined. Flash LED PWM frequency
-#define CONFIG_FLASH_PWM_BITS    9                //   - Mandatory if CONFIG_LED_ILLUMINATOR_ENABLED defined. Resolution of duty cycle counter
-#define CONFIG_LED_MAX_INTENSITY 100              //   - Mandatory if CONFIG_LED_ILLUMINATOR_ENABLED defined. A percentage (0..100) of full intensity
+#if defined(CONFIG_FLASH_LED)
+#define CONFIG_LED_ILLUMINATOR_ENABLED
+#define CONFIG_FLASH_PWM_FREQ    50000            // Flash LED PWM frequency
+#define CONFIG_FLASH_PWM_BITS    9                // Resolution of duty cycle counter
+#define CONFIG_LED_MAX_INTENSITY 100              // A percentage (0..100) of full intensity
+#if !defined(CONFIG_LED_LEDC_CHANNEL)
+  #error "Must specify LEDC channel"
+#endif
+#endif
+
+// sanity checks
+#if defined(CONFIG_LED_LEDC_CHANNEL) && !defined(CONFIG_FLASH_LED)
+  #error "Flash LED pin must be defined"
+#endif
+
+#if defined(CONFIG_ESP_FACE_RECOGNITION_ENABLED) && !defined(CONFIG_ESP_FACE_DETECT_ENABLED)
+  #define CONFIG_ESP_FACE_DETECT_ENABLED
+#endif
+
+#if defined(CONFIG_LED_MAX_INTENSITY) && (CONFIG_LED_MAX_INTENSITY > 100)
+  #undef CONFIG_LED_MAX_INTENSITY
+  #define CONFIG_LED_MAX_INTENSITY 100
 #endif
 
 #endif // #ifndef CONFIG_H
